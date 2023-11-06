d13209dad6668a500b3bb89acef84ad8cbc642b3
Nicolai Schneider <nicolai-schneider@t-online.de>
2023-11-06 20:28:14 +0100

    added unsafe constraints

---
+++ Sources/SimpleConstraints/ConstraintAnchorUnsafe.swift
@@ -0,0 +1,129 @@
+import UIKit
+
+extension SimpleConstraints {
+    
+    public enum ConsraintTypeUnsafe {
+        case top, bottom, left, right
+        case centerX, centerY
+        case height, width
+        case ratioHeight, ratioWidth
+    }
+    
+    public enum ConsraintAnchorUnsafe {
+        
+        case top(UIView, CGFloat, safe: Bool = false)
+        case bottom(UIView, CGFloat, safe: Bool = false)
+        case left(UIView, CGFloat, safe: Bool = false)
+        case right(UIView, CGFloat, safe: Bool = false)
+        case centerX(UIView, CGFloat)
+        case centerY(UIView, CGFloat)
+        case length(CGFloat)
+        case ratio(CGFloat)
+                
+        var constant: CGFloat {
+            switch self {
+            case .top(_, let constant, _):
+                return constant
+            case .bottom(_, let constant, _):
+                return constant
+            case .left(_, let constant, _):
+                return constant
+            case .right(_, let constant, _):
+                return constant
+            case .centerX(_, let constant):
+                return constant
+            case .centerY(_, let constant):
+                return constant
+            case .length, .ratio:
+                return 0
+            }
+        }
+        
+        var heightOrWidth: CGFloat? {
+            switch self {
+            case .length(let length):
+                return length
+            case .left, .right, .centerX, .centerY, .top, .bottom, .ratio:
+                return nil
+            }
+        }
+        
+        var ratio: CGFloat? {
+            switch self {
+            case .ratio(let ratio):
+                return ratio
+            case .left, .right, .centerX, .centerY, .top, .bottom, .length:
+                return nil
+            }
+        }
+        
+        var anchorY: NSLayoutYAxisAnchor? {
+            switch self {
+            case .top(let view, _, safe: let safe):
+                return safe ?
+                view.safeAreaLayoutGuide.topAnchor :
+                view.topAnchor
+            case .bottom(let view, _, safe: let safe):
+                return safe ?
+                view.safeAreaLayoutGuide.bottomAnchor :
+                view.bottomAnchor
+            case .left, .right, .centerX, .centerY, .length, .ratio:
+                return nil
+            }
+        }
+        
+        var anchorX: NSLayoutXAxisAnchor? {
+            switch self {
+            case .left(let view, _, safe: let safe):
+                return safe ?
+                view.safeAreaLayoutGuide.leftAnchor :
+                view.leftAnchor
+            case .right(let view, _, safe: let safe):
+                return safe ?
+                view.safeAreaLayoutGuide.rightAnchor :
+                view.rightAnchor
+            case .top, .bottom, .centerX, .centerY, .length, .ratio:
+                return nil
+            }
+        }
+        
+        var centerX: NSLayoutXAxisAnchor? {
+            switch self {
+            case .centerX(let view, _):
+                return view.centerXAnchor
+            case .top, .bottom, .left, .right, .centerY, .length, .ratio:
+                return nil
+            }
+        }
+        
+        var centerY: NSLayoutYAxisAnchor? {
+            switch self {
+            case .centerY(let view, _):
+                return view.centerYAnchor
+            case .top, .bottom, .left, .right, .centerX, .length, .ratio:
+                return nil
+            }
+        }
+        
+        var description: String {
+            switch self {
+            case .top:
+                return "Top Anchor"
+            case .bottom:
+                return "Bottom Anchor"
+            case .left:
+                return "Left Anchor"
+            case .right:
+                return "Right Anchor"
+            case .centerX:
+                return "CenterX Anchor"
+            case .centerY:
+                return "CenterY Anchor"
+            case .length:
+                return "Width or Height Anchor"
+            case .ratio:
+                return "Width or Height Anchor (Aspect Ratio)"
+            }
+        }
+    }
+}
--- Sources/SimpleConstraints/Extensions/ConstraintHelperUnsafe/UIView+UnsafeConstraints.swift
+++ Sources/SimpleConstraints/Extensions/ConstraintHelperUnsafe/UIView+UnsafeConstraints.swift
@@ -3,11 +3,96 @@ import UIKit
 public extension UIView {
     
     // TODO: add function for unsafe constraints (parameter becomes array of contraints)
-    /*
-     func edgesUnsafe(
-     _ subView: UIView,
-     edges: [ConstraintEdgeDistances: ConstraintAnchor],
-     height: CGFloat
-     ) { }
-     */
+    func unsafeConstraints(
+        _ subView: UIView,
+        constraints: [(SimpleConstraints.ConsraintTypeUnsafe, SimpleConstraints.ConsraintAnchorUnsafe)],
+        addSubView: Bool = true,
+        translatesAutoresizingMaskIntoConstraints: Bool = false
+    ) {
+        commonSetup(subView, sub: addSubView, translate: translatesAutoresizingMaskIntoConstraints)
+        for constraint in constraints {
+            classifyAndSetConstraint(subView: subView, constraint: constraint)
+        }
+    }
+}
+
+extension UIView {
+    
+    private func classifyAndSetConstraint(
+        subView: UIView,
+        constraint: (SimpleConstraints.ConsraintTypeUnsafe, SimpleConstraints.ConsraintAnchorUnsafe)
+    ) {
+        let anchorToSet = constraint.1
+        switch constraint.0 {
+        case .top:
+            guard let anchor = anchorToSet.anchorY else {
+                debugPrint("Top Anchor can't set \(anchorToSet.description) for sub view.")
+                return
+            }
+            subView.topAnchor.constraint(equalTo: anchor, constant: anchorToSet.constant).isActive = true
+            
+        case .bottom:
+            guard let anchor = anchorToSet.anchorY else {
+                debugPrint("Bottom Anchor can't set \(anchorToSet.description) for sub view.")
+                return
+            }
+            subView.bottomAnchor.constraint(equalTo: anchor, constant: anchorToSet.constant).isActive = true
+            
+        case .left:
+            guard let anchor = anchorToSet.anchorX else {
+                debugPrint("Left Anchor can't set \(anchorToSet.description) for sub view.")
+                return
+            }
+            subView.leftAnchor.constraint(equalTo: anchor, constant: anchorToSet.constant).isActive = true
+            
+        case .right:
+            guard let anchor = anchorToSet.anchorX else {
+                debugPrint("Right Anchor can't set \(anchorToSet.description) for sub view.")
+                return
+            }
+            subView.rightAnchor.constraint(equalTo: anchor, constant: anchorToSet.constant).isActive = true
+            
+        case .centerX:
+            guard let anchor = anchorToSet.centerX else {
+                debugPrint("CenterX Anchor can't set \(anchorToSet.description) for sub view.")
+                return
+            }
+            subView.centerXAnchor.constraint(equalTo: anchor, constant: anchorToSet.constant).isActive = true
+            
+        case .centerY:
+            guard let anchor = anchorToSet.centerY else {
+                debugPrint("CenterY Anchor can't set \(anchorToSet.description) for sub view.")
+                return
+            }
+            subView.centerYAnchor.constraint(equalTo: anchor, constant: anchorToSet.constant).isActive = true
+            
+        case .height:
+            guard let height = anchorToSet.heightOrWidth else {
+                debugPrint("Height Anchor can't set \(anchorToSet.description) for sub view.")
+                return
+            }
+            subView.heightAnchor.constraint(equalToConstant: height).isActive = true
+            
+        case .width:
+            guard let width = anchorToSet.heightOrWidth else {
+                debugPrint("Height Anchor can't set \(anchorToSet.description) for sub view.")
+                return
+            }
+            subView.heightAnchor.constraint(equalToConstant: width).isActive = true
+        
+        case .ratioHeight:
+            guard let ratio = anchorToSet.ratio else {
+                debugPrint("Height Anchor can't set \(anchorToSet.description) for sub view.")
+                return
+            }
+            subView.heightAnchor.constraint(equalTo: subView.widthAnchor, multiplier: ratio).isActive = true
+            
+        case .ratioWidth:
+            guard let ratio = anchorToSet.ratio else {
+                debugPrint("Height Anchor can't set \(anchorToSet.description) for sub view.")
+                return
+            }
+            subView.widthAnchor.constraint(equalTo: subView.heightAnchor, multiplier: ratio).isActive = true
+        }
+    }
 }
